e418cdaf6fb6b4f1bb9616a767ecd6cc51e93a1e
Muhammet Şirin <muhammetsirin07@gmail.com>
2021-08-10 09:57:53 +0300

    MVC PROJE KAMPİ FİNAL

---
+++ MVCProjeKampi/Controllers/AuthorizationController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKampi.Controllers
+{
+    [AllowAnonymous]
+    public class AuthorizationController : Controller
+    {
+        // GET: Authorization
+        AdminManager adminmanager = new AdminManager(new EfAdminDal());
+        public ActionResult Index()
+        {
+            var adminvalues = adminmanager.GetList();
+            return View(adminvalues);
+        }
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAdmin(Admin admin)
+        {
+            adminmanager.AdminAdd(admin);
+            return RedirectToAction("Index");
+          
+        }
+        [HttpGet]
+        public ActionResult EditAdmin(int id)
+        {
+            var adminvalue = adminmanager.GetByID(id);
+            return View(adminvalue);
+        }
+        [HttpPost]
+        public ActionResult EditAdmin(Admin p)
+        {
+            adminmanager.AdminUpdate(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCProjeKampi/Controllers/ContentController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Concrete;
+
+namespace MVCProjeKampi.Controllers
+{
+    public class ContentController : Controller
+    {
+        // GET: Content
+        ContentManager cm = new ContentManager(new EfContentDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+        Context c = new Context();
+        public ActionResult GetAllContent(string p)
+        {
+
+            var values = cm.GetList(p);
+          
+            return View(values);
+        }
+        public ActionResult ContentbyHeading(int id)
+        {
+            var contentvalues = cm.GetListByHeadingID(id);
+            return View(contentvalues);
+        }
+    }
+}
